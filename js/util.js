c658663b952f5b213809a433349e8ca778be2521
SunnySan <sunny1227@pchome.com.tw>
2019-08-13 10:03:29 +0800

    office

--- js/util.js
+++ js/util.js
@@ -133,10 +133,42 @@ function unBlockUI(){
 	$.unblockUI();
 }
 
-/**********取得今天日期，格式為：2013/10/3**********/
+
+
+
+
+
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小時 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
+/**********取得今天日期，格式為：2013-10-3**********/
 function getCurrentDate(){
 	var currDate = new Date();	//目前時間
-	var txtCurrDate = currDate.getFullYear() + "-" + (currDate.getMonth()+1) + "-" + currDate.getDate();	//今天日期，格式為：2013/10/3
+	//var txtCurrDate = currDate.getFullYear() + "-" + (currDate.getMonth()+1) + "-" + currDate.getDate();	//今天日期，格式為：2013/10/3
+	var txtCurrDate = currDate.Format("yyyy-MM-dd");	//今天日期，格式為：2013/10/3
+	return txtCurrDate;
+}
+
+/**********取得前一天日期，格式為：2013-10-3**********/
+function getPreviousDate(){
+	var currDate = new Date();	//目前時間
+	var before = currDate-1000*60*60*24;
+	var beforeDate = new Date(before);
+	//var txtCurrDate = beforeDate.getFullYear() + "-" + (beforeDate.getMonth()+1) + "-" + beforeDate.getDate();	//今天日期，格式為：2013/10/3
+	var txtCurrDate = beforeDate.Format("yyyy-MM-dd");	//今天日期，格式為：2013/10/3
 	return txtCurrDate;
 }
 
